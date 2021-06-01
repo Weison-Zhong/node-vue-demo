a9f5eee089c1b7732b79a7beca01d13f533b5836
Weison-Zhong <jmzws@qq.com>
2021-06-01 13:51:40 +0800

    submit2

--- web/src/views/Article.vue
+++ web/src/views/Article.vue
@@ -1,13 +1,22 @@
 <template>
   <div class="page-article" v-if="model">
-    <div class="d-flex py-3 px-2">
-      <div class="iconfont icon-back-line"></div>
-      <strong class="flex-1">
+    <div class="d-flex py-3 px-2 border-bottom">
+      <div class="iconfont icon-back-line text-blue"></div>
+      <strong class="flex-1 text-blue pl-2">
         {{ model.title }}
       </strong>
       <div class="text-grey fs-xs">2021-05-31</div>
     </div>
     <div v-html="model.body" class="px-3 body fs-lg"></div>
+    <div class="px-3 border-top py-2">
+      <div class="d-flex ai-center">
+        <i class="iconfont icon-menu"></i>
+        <strong class="text-blue fs-lg ml-1">相关资讯</strong>
+      </div>
+    </div class="pt-2">
+    <router-link class="py-2" tag="div" to="`/articles/${item._id}`"  v-for="item in model.related" :key="item._id">{{
+      item.title
+    }}</router-link>
   </div>
 </template>
 
@@ -45,5 +54,8 @@ export default {
       height: auto;
     }
   }
+}
+.icon-back-line {
+  font-size: 1.6923rem;
 }
 </style>
